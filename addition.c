6cf621306e62abd14bd6c77709da08fa5493b974
nagpojari <53028898+nagpojari@users.noreply.github.com>
2019-07-18 09:22:05 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ printf("Hello,World!!!");
+ printf("Simple addition and subtraction in c language");
+ int a,b,c,d;
+ printf("Enter value of a : "):
+ scanf("%d",&a);
+ printf("Enter value of b : ");
+ scanf("%d",&b);
+ c=a+b;
+ d=a-b;
+ printf("The addition of two number is : %d",c);
+ printf("The difference of two number is : %d",d);
+}
